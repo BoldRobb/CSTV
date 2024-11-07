6867da30c3523e64174ea2da5c22ba28032858ff
geovanyayala <geovanyayala@gmail.com>
2024-11-07 08:11:09 -0700

    Update app.component.ts
    quité animations, porque ya estaba en otro archivo

--- Page/src/app/app.component.ts
+++ Page/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { RecentActivityComponent } from './components/foros/recent-activity/recent-activity.component';
 import { ScoreboardComponent } from './components/live/scoreboard/scoreboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, RecentActivityComponent,ScoreboardComponent],
-  providers: [provideAnimations()],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
